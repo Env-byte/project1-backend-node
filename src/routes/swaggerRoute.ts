fd0a201f3cfc6ccb6024ee2fe74978b60bdf33dd
Env-byte <tompenn18@gmail.com>
2022-10-18 11:37:14 +0100

    Removed docs folder using postman folder for yaml open api spec

--- src/routes/swaggerRoute.ts
+++ src/routes/swaggerRoute.ts
@@ -2,16 +2,16 @@ import * as core from "express-serve-static-core";
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 import fs from "fs";
+
 const YAML = require('yamljs');
 const router: core.Router = express.Router();
-const path = '../../postman/schemas/index.yaml';
-let yaml;
+const path = './postman/schemas/openapi.yaml';
 if (fs.existsSync(path)) {
-    yaml = fs.readFileSync(path);
-    const swaggerDocument = YAML.parse(path);
-    console.log(swaggerDocument)
+    const swaggerDocument = YAML.load(path);
     router.use('/docs', swaggerUi.serve);
     router.get('/docs', swaggerUi.setup(swaggerDocument));
+} else {
+    console.log(`does not exist ${path}`)
 }
 
 export default router; // export to use in server.js
